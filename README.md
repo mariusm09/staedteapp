529151645615092b231f73757cdcfa9f20448e23
David Janssen <david.janssen@phlu.ch>
2015-07-03 13:42:28 +0200

    added offline-support for firebase, added Weather-forecast

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# staedteapp
+ # Städte-App
+Ionic based App
+
+***
+
+## Launch APP on Android device
+```sh
+$ ionic run android
+```
+
+***
+
+## Launch APP on iOS device
+```sh
+$ ionic run ios
+```
--- www/js/factory.js
+++ www/js/factory.js
@@ -1,8 +1,31 @@
 ﻿angular.module('starter.factory', [])
 
-.factory('DataFactory', function ($firebaseArray) {
+.factory('DataFactory', function ($firebaseArray, $rootScope, $timeout) {
 
-    var ref = new Firebase("https://luminous-torch-3697.firebaseio.com/cities");
+    OfflineFirebase.restore();
+
+    // Dave Daten
+    //var ref = new OfflineFirebase("https://sizzling-torch-9896.firebaseio.com/cities");
+
+    // Marius Daten
+    var ref = new OfflineFirebase("https://luminous-torch-3697.firebaseio.com/cities");
+
+    ref.on('value', function(snapshot) {
+        if($rootScope.newdata == null){
+            $rootScope.newdata = 0;
+        } else {
+            $rootScope.newdata ++;
+
+            if ($rootScope.newdata > 1) {
+
+            $timeout(function () {
+                $rootScope.newdata --;
+            }, 3000);
+            }
+        }
+        console.log("data changed", snapshot.val());
+    }, undefined, undefined, true);
+
     // download the data into a local object
     var cities = $firebaseArray(ref);
 
@@ -20,3 +43,40 @@
         }
     }
 })
+
+.factory('Openweathermap', function($http){
+    var apiUrl = 'http://api.openweathermap.org/data/2.5/weather';
+
+    var openweathermapService = {};
+
+    openweathermapService.getWeatherByLocationString = function(locationString, callbackSuccess, callbackError) {
+        $http({
+            method: 'GET',
+            url: apiUrl,
+            params: {
+                q: locationString
+            }
+        }).success(callbackSuccess).error(callbackError);
+    };
+
+    return openweathermapService;
+})
+
+.factory('Openweatherforecast', function($http){
+    var apiUrl = 'http://api.openweathermap.org/data/2.5/forecast/daily';
+
+    var openweatherforecastService = {};
+
+    openweatherforecastService.getWeatherByLocationString = function(locationString, callbackSuccess, callbackError) {
+        $http({
+            method: 'GET',
+            url: apiUrl,
+            params: {
+                q: locationString,
+                cnt: 7
+            }
+        }).success(callbackSuccess).error(callbackError);
+    };
+
+    return openweatherforecastService;
+});
